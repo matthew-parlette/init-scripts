a32b6078e1e884d74b081c7887cf6547aa269b8f
Matt Parlette <matthew.parlette@gmail.com>
2013-10-25 06:49:32 -0400

    Add puppet_setup.sh
    This script will install puppet and run it for the first time to get the initial configuration from the puppet master. Once that is complete, it then enables the puppet agent to run through /etc/default/puppet. All other puppet related configuration (such as puppet.conf) comes from the puppet master.

---
+++ puppet_setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#Verify if root
+if [ "$(whoami)" != "root" ]; then
+  echo "This program must be run as root, try 'sudo $0'"
+  exit 1
+fi
+reboot=0
+puppetmaster="puppet-master.andromeda"
+hostname=`hostname`
+
+#Make sure the hostname ends in .andromeda
+if [ "$hostname" != *.andromeda ]; then
+  hostname="$hostname.andromeda"
+fi
+echo "*Hostname set to $hostname*"
+
+read -r -p "Configure puppet? [Y/n] " response
+response=${response,,}
+if [[ "$response" =~ ^(yes|y|)$ ]]; then
+  #Install puppet
+  echo "Installing puppet..."
+  apt-get install -qq puppet
+  if [ ! "$?" ]; then
+    echo -n "Puppet failed to install, exiting..."
+    exit 1
+  fi
+
+  echo "Verifying connectivity to puppet master ($puppetmaster)..."
+  ping -c 1 "$puppetmaster" > /dev/null
+  if [ ! "$?" ]; then
+    echo -n "$puppetmaster did not respond to ping, exiting..."
+    exit 1
+  fi
+
+  #Run first time
+  echo "Executing first run of puppet agent..."
+  echo "** Check to see if the cert needs to be authenticated on the puppet master **"
+  puppet agent --test --server "$puppetmaster" --waitforcert 10 --certname "$hostname"
+
+  if [ ! "$?" ]; then
+    echo -n "Puppet agent first time run failed, exiting..."
+    exit 1
+  fi
+
+  echo "Starting puppet agent..."
+  sed -i 's/START=no/START=yes/g' /etc/default/puppet
+  #sed -i s/'^DAEMON_OPTS=.*$'/"DAEMON_OPTS=\"--certname $hostname\""/g /etc/default/puppet
+  #puppet agent --enable
+
+fi
+
+
+#echo -n "n2n IP: "
+#read n2n_ip
+#sed -i s/"ip_addr"/"$n2n_ip"/g /etc/init/n2n.conf
+
+echo "| Puppet config done |"
+if [ $reboot -eq 1 ]; then
+  echo "| Reboot before use  |"
+fi
