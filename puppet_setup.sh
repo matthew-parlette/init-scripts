fbabe52ef4ffcb3625e0c407287d1b43f05cb398
Matt Parlette <matthew.parlette@gmail.com>
2014-01-24 10:22:37 -0500

    Add puppet master to hosts file automatically.
    Also fix some more indenting.

--- puppet_setup.sh
+++ puppet_setup.sh
@@ -13,6 +13,12 @@ if [ "$hostname" != *.andromeda ]; then
 fi
 echo "*Hostname set to $hostname*"
 
+#Make sure puppetmaster address is in hosts file
+hosts_entry=`cat /etc/hosts | grep $puppetmaster`
+if [ -z "$hosts_entry" ]; then
+  echo "10.1.2.201\tpuppet-master.andromeda" >> /etc/hosts
+fi
+
 #Install puppet
 echo "Installing puppet..."
 apt-get install -qq puppet
@@ -24,8 +30,8 @@ fi
 echo "Verifying connectivity to puppet master ($puppetmaster)..."
 ping -c 1 "$puppetmaster" > /dev/null
 if [ ! "$?" ]; then
-echo -n "$puppetmaster did not respond to ping, exiting..."
-exit 1
+  echo -n "$puppetmaster did not respond to ping, exiting..."
+  exit 1
 fi
 
 #Run first time
@@ -34,13 +40,13 @@ echo "** Check to see if the cert needs to be authenticated on the puppet master
 puppet agent --test --server "$puppetmaster" --waitforcert 10 --certname "$hostname"
 
 if [ ! "$?" ]; then
-echo -n "Puppet agent first time run failed, exiting..."
-exit 1
+  echo -n "Puppet agent first time run failed, exiting..."
+  exit 1
 fi
 
 echo "Starting puppet agent..."
 if [ -a "/etc/default/puppet" ]; then
-sed -i 's/START=no/START=yes/g' /etc/default/puppet
+  sed -i 's/START=no/START=yes/g' /etc/default/puppet
 fi
 #sed -i s/'^DAEMON_OPTS=.*$'/"DAEMON_OPTS=\"--certname $hostname\""/g /etc/default/puppet
 puppet agent --enable
