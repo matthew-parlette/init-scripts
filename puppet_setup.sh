4c7790cd7160019dbf7f23899af66d2b8aeebdc1
Matt Parlette <matthew.parlette@gmail.com>
2014-01-24 07:57:57 -0500

    Remove questions so it can be run by initial_setup

--- puppet_setup.sh
+++ puppet_setup.sh
@@ -4,7 +4,6 @@ if [ "$(whoami)" != "root" ]; then
   echo "This program must be run as root, try 'sudo $0'"
   exit 1
 fi
-reboot=0
 puppetmaster="puppet-master.andromeda"
 hostname=`hostname`
 
@@ -14,49 +13,34 @@ if [ "$hostname" != *.andromeda ]; then
 fi
 echo "*Hostname set to $hostname*"
 
-read -r -p "Configure puppet? [Y/n] " response
-response=${response,,}
-if [[ "$response" =~ ^(yes|y|)$ ]]; then
-  #Install puppet
-  echo "Installing puppet..."
-  apt-get install -qq puppet
-  if [ ! "$?" ]; then
-    echo -n "Puppet failed to install, exiting..."
-    exit 1
-  fi
-
-  echo "Verifying connectivity to puppet master ($puppetmaster)..."
-  ping -c 1 "$puppetmaster" > /dev/null
-  if [ ! "$?" ]; then
-    echo -n "$puppetmaster did not respond to ping, exiting..."
-    exit 1
-  fi
-
-  #Run first time
-  echo "Executing first run of puppet agent..."
-  echo "** Check to see if the cert needs to be authenticated on the puppet master **"
-  puppet agent --test --server "$puppetmaster" --waitforcert 10 --certname "$hostname"
-
-  if [ ! "$?" ]; then
-    echo -n "Puppet agent first time run failed, exiting..."
-    exit 1
-  fi
-
-  echo "Starting puppet agent..."
-  if [ -a "/etc/default/puppet" ]; then
-    sed -i 's/START=no/START=yes/g' /etc/default/puppet
-  fi
-  #sed -i s/'^DAEMON_OPTS=.*$'/"DAEMON_OPTS=\"--certname $hostname\""/g /etc/default/puppet
-  puppet agent --enable
-
+#Install puppet
+echo "Installing puppet..."
+apt-get install -qq puppet
+if [ ! "$?" ]; then
+echo -n "Puppet failed to install, exiting..."
+exit 1
 fi
 
-
-#echo -n "n2n IP: "
-#read n2n_ip
-#sed -i s/"ip_addr"/"$n2n_ip"/g /etc/init/n2n.conf
-
-echo "| Puppet config done |"
-if [ $reboot -eq 1 ]; then
-  echo "| Reboot before use  |"
+echo "Verifying connectivity to puppet master ($puppetmaster)..."
+ping -c 1 "$puppetmaster" > /dev/null
+if [ ! "$?" ]; then
+echo -n "$puppetmaster did not respond to ping, exiting..."
+exit 1
 fi
+
+#Run first time
+echo "Executing first run of puppet agent..."
+echo "** Check to see if the cert needs to be authenticated on the puppet master **"
+puppet agent --test --server "$puppetmaster" --waitforcert 10 --certname "$hostname"
+
+if [ ! "$?" ]; then
+echo -n "Puppet agent first time run failed, exiting..."
+exit 1
+fi
+
+echo "Starting puppet agent..."
+if [ -a "/etc/default/puppet" ]; then
+sed -i 's/START=no/START=yes/g' /etc/default/puppet
+fi
+#sed -i s/'^DAEMON_OPTS=.*$'/"DAEMON_OPTS=\"--certname $hostname\""/g /etc/default/puppet
+puppet agent --enable
